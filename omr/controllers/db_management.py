8959706f227308521cc8052df7dcd2a9c0b81c83
Ronikos <theo.cftw@gmail.com>
2016-11-20 22:34:11 +0000

    v0.4.0-0.3
    sqlite3 is the one

--- omr/controllers/db_management.py
+++ omr/controllers/db_management.py
@@ -1,33 +1,104 @@
-import csv
+import sqlite3
 
 
-class Table(object):
-    def __init__(self, name, table_type):
-        self.name = name
-        self.fieldnames = self.choose_field_names(table_type)
+def scrub(table_name):
+    if isinstance(table_name, str):
+        return ''.join(chr for chr in table_name if chr.isalnum() or chr == " ")
+    else:
+        return table_name
 
-    def choose_field_names(self, table_type):
-        if table_type == "class":
-            return ["Student_ID", "Student_Name"]
-        elif table_type == "results":
-            return ["Class_Name", "Test_ID", "Student_ID", "Score"]
-        elif table_type == "results":
-            return ["Class_Name", "Test_ID", "Student_ID", "Score"]
 
-    def create_table(self):
-        with open("../data/" + self.name + ".csv", "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+def generate_database():
+    conn = sqlite3.connect("../data/database.db")
 
-            writer.writeheader()
+    c = conn.cursor()
 
-    def add_to_table(self, data):
-        """
-        PARAMS: student_data: {Student_ID: INT, Student_Name: STRING}
-                results_data: {Class_Name: STRING, Student_ID: INT, Test_ID: INT, Score:INT}
-                test_data:    {Test_ID: INT, Test_Name: STRING, Max_Mark: INT}
-        """
+    c.execute("create table if not exists Classes ("
+              "Class_ID INTEGER PRIMARY KEY, "
+              "Class_Name TEXT)")
 
-        with open("../data/" + self.name + ".csv", "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.name)
+    c.execute("create table if not exists Students ("
+              "Student_ID INTEGER PRIMARY KEY, "
+              "Student_Name TEXT, "
+              "Class_ID INTEGER)")
 
-            writer.writerow(data)
+    c.execute("create table if not exists Results ("
+              "Results_ID INTEGER PRIMARY KEY, "
+              "Student_ID TEXT, "
+              "Score INTEGER, "
+              "Test_ID INTEGER)")
+
+    c.execute("create table if not exists Tests ("
+              "Test_ID INTEGER PRIMARY KEY, "
+              "Test_Name TEXT, "
+              "Max_score INTEGER)")
+
+    conn.close()
+
+
+def add_class(class_data):
+    if not isinstance(class_data, tuple) or len(class_data) != 1:
+        raise TypeError("Expected 1-tuple")
+
+    conn = sqlite3.connect("../data/database.db")
+
+    c = conn.cursor()
+
+    class_data = tuple([scrub(i) for i in list(class_data)])
+    print(class_data)
+
+    c.execute('INSERT INTO Classes(Class_Name) VALUES (?)', class_data)
+
+    conn.commit()
+    conn.close()
+
+
+def add_student(student_data):
+    if not isinstance(student_data, tuple) or len(student_data) != 2:
+        raise TypeError("Expected 2-tuple")
+
+    conn = sqlite3.connect("../data/database.db")
+
+    c = conn.cursor()
+
+    student_data = tuple([scrub(i) for i in list(student_data)])
+    print(student_data)
+
+    c.execute('INSERT INTO Students(Student_Name, Class_ID) VALUES (?,?)', student_data)
+
+    conn.commit()
+    conn.close()
+
+
+def add_result(result_data):
+    if not isinstance(result_data, tuple) or len(result_data) != 3:
+        raise TypeError("Expected 3-tuple")
+
+    conn = sqlite3.connect("../data/database.db")
+
+    c = conn.cursor()
+
+    result_data = tuple([scrub(i) for i in list(result_data)])
+    print(result_data)
+
+    c.execute('INSERT INTO Results(Student_ID, Score, Test_ID) VALUES (?,?, ?)', result_data)
+
+    conn.commit()
+    conn.close()
+
+
+def add_test(test_data):
+    if not isinstance(test_data, tuple) or len(test_data) != 2:
+        raise TypeError("Expected 2-tuple")
+
+    conn = sqlite3.connect("../data/database.db")
+
+    c = conn.cursor()
+
+    test_data = tuple([scrub(i) for i in list(test_data)])
+    print(test_data)
+
+    c.execute('INSERT INTO Tests(Test_Name, Max_score) VALUES (?,?)', test_data)
+
+    conn.commit()
+    conn.close()
