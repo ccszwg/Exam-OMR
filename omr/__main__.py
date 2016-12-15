96cf10495906f80d4e3b47e2336ce4f58e287491
Ronikos <theo.cftw@gmail.com>
2016-12-15 18:47:48 +0000

    v1.0.0
    INITIAL RELEASE
    If any problems are found, please let me know. Still has bugs but are not software breaking

--- omr/__main__.py
+++ omr/__main__.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
-from controllers.GUI import MainWindow
+from src.GUI import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
