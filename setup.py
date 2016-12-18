ca5e1a24afaff76438e4e79bdaaa92ddd04bf0f5
Ronikos <theo.cftw@gmail.com>
2016-12-18 12:21:33 +0000

    v1.0.1
    setup.py added

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-from distutils.core import setup
+import pip
+from setuptools import setup
 
-# todo: test (OpenCV probably doesn't work)
+dependancies = ['comtypes', 'docx', 'qrcode', 'PyPDF2', 'pyqtgraph', 'PyQt5', 'numpy', 'Pillow', 'opencv-python',
+                'wheel']
 
-setup(
-    name='OMR',
-    version='',
-    packages=['omr.src', 'omr.interface'],
-    url='',
-    license='',
-    author='Theo Charalambous',
-    author_email='theo.cftw@gmail.com',
-    description='',
-    requires=['comtypes', 'docx', 'qrcode', 'PyPDF2', 'pyqtgraph', 'PyQt5', 'numpy', 'PIL', 'cv2']
-)
+
+def install():
+    try:
+        for package in dependancies:
+            pip.main(['install', package])
+
+        setup(
+            name='OMR',
+            version='1.0.0',
+            packages=['omr.src', 'omr.interface', 'omr.resources'],
+            url='',
+            license='',
+            author='Theo Charalambous',
+            author_email='theo.cftw@gmail.com',
+            description='',
+            tests_require=dependancies
+        )
+    except Exception as e:
+        print(e)
+
+
+install()
